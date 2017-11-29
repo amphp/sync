0886f9cdc2ee549b168e69de6997c30e583e5942
Aaron Piotrowski <aaron@trowski.com>
2017-11-29 00:12:42 -0600

    Add test for synchronized function

---
+++ test/SynchronizedTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Amp\Sync\Test;
+
+use Amp\Delayed;
+use Amp\Loop;
+use Amp\PHPUnit\TestCase;
+use Amp\Sync\LocalMutex;
+use function Amp\Sync\synchronized;
+
+class SynchronizedTest extends TestCase {
+    public function testSynchronized() {
+        $this->assertRunTimeGreaterThan(function () {
+            Loop::run(function () {
+                $mutex = new LocalMutex;
+                $callback = function (int $value) {
+                    return yield new Delayed(100, $value);
+                };
+
+                $promises = [];
+                foreach (\range(0, 2) as $value) {
+                    $promises[] = synchronized($mutex, $callback, $value);
+                }
+                $result = yield $promises;
+                $this->assertSame(\range(0, 2), $result);
+            });
+        }, 300);
+    }
+}
