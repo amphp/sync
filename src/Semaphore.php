7412e5bbd7620535182cf057fe44aab1f9466043
Niklas Keller <me@kelunik.com>
2019-11-07 22:52:05 +0100

    Fix interface extension

--- src/Semaphore.php
+++ src/Semaphore.php
@@ -10,12 +10,12 @@ use Amp\Promise;
  * Objects that implement this interface should guarantee that all operations are atomic. Implementations do not have to
  * guarantee that acquiring a lock is first-come, first serve.
  */
-interface Semaphore extends Mutex
+interface Semaphore
 {
     /**
      * Acquires a lock on the semaphore.
      *
-     * @return \Amp\Promise<\Amp\Sync\Lock> Resolves with an integer keyed lock object. Identifiers returned by the
+     * @return Promise<Lock> Resolves with an integer keyed lock object. Identifiers returned by the
      *    locks should be 0-indexed. Releasing an identifier MUST make that same identifier available. May fail with
      *    a SyncException if an error occurs when attempting to obtain the lock (e.g. a shared memory segment closed).
      */
