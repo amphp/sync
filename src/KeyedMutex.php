5231b8baf7ec191516d3c8efd41f3c28d6d4b08e
Niklas Keller <me@kelunik.com>
2021-12-06 00:13:39 +0100

    Drop non-blocking from KeyedSemaphore docs

--- src/KeyedMutex.php
+++ src/KeyedMutex.php
@@ -3,7 +3,7 @@
 namespace Amp\Sync;
 
 /**
- * A non-blocking synchronization primitive that can be used for mutual exclusion across contexts based on keys.
+ * A synchronization primitive that can be used for mutual exclusion across contexts based on keys.
  *
  * Objects that implement this interface should guarantee that all operations are atomic. Implementations do not have to
  * guarantee that acquiring a lock is first-come, first serve.
--- src/KeyedSemaphore.php
+++ src/KeyedSemaphore.php
@@ -3,7 +3,7 @@
 namespace Amp\Sync;
 
 /**
- * A non-blocking counting semaphore based on keys.
+ * A counting semaphore based on keys.
  *
  * Objects that implement this interface should guarantee that all operations are atomic. Implementations do not have to
  * guarantee that acquiring a lock is first-come, first serve.
